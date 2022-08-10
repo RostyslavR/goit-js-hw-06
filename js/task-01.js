a154821b946284cba1a805030eb305c08ebe912e
RostyslavR <rrmail@ukr.net>
2022-08-10 20:49:30 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,4 @@
+const refCategories = document.getElementById("categories");
+const refCategoryList = document.querySelectorAll(".item");
+console.log(refCategories);
+console.log(`Number of categories: ${refCategoryList.length}`);
