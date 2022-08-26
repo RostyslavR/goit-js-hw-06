31790bc5c0c37ef964534751406b14ccd20ac608
unknown <rrmail@ukr.net>
2022-08-26 09:23:12 +0300

    upd

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,9 @@
+document.querySelector(".change-color").addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  changeBodyColor(newColor);
+  changeTextSpan(newColor);
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -9,9 +15,3 @@ function changeBodyColor(newColor) {
 function changeTextSpan(newColor) {
   document.querySelector(".color").textContent = "-" + newColor;
 }
-
-document.querySelector(".change-color").addEventListener("click", () => {
-  const newColor = getRandomHexColor();
-  changeBodyColor(newColor);
-  changeTextSpan(newColor);
-});
